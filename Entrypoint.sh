731bea502f91b5305f16bfcad6435b4a9fca1e4c
sakkuntyo <20591351+sakkuntyo@users.noreply.github.com>
2025-07-15 00:28:46 +0900

    Update Entrypoint.sh

--- Entrypoint.sh
+++ Entrypoint.sh
@@ -2,15 +2,23 @@
 
 set -e
 
-# インストール準備
+# pre install
 apt update;apt install xz-utils wget
 
-# インストール
+# install 
 cd /root
 wget https://www.factorio.com/get-download/2.0.55/headless/linux64 -O ./factrioserver.tar.xz
 unxz ./factrioserver.tar.xz
 tar -xvf ./factrioserver.tar.xz
 
-# mods-list 書き換え
+# replace mods-list.json
+wget https://raw.githubusercontent.com/sakkuntyo/docker-factorio-server/refs/heads/2.0.55/mod-list.json -O ./vanila-mod-list.json
+cat ./vanila-mod-list.json
+cp ./vanila-mod-list.json /root/factorio/mods/mod-list.json
+
+# post install
 cd /root/factorio/bin/x64
-cp 
+test -f /root/factoriodata/mysave.zip && echo "not initialized because exist root/factoriodata/mysave.zip. " || ./factorio --create /root/factoriodata/mysave
+
+# launch
+./factorio --start-server /root/corekeeperdata/mysave.zip
